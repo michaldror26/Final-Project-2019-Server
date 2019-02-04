78595024214922f0abddb35d7278804a490c65ce
Yehudit Ravina <michaldror26@gmail.com>
2019-02-04 14:54:40 +0200

    Merge branch 'master' of https://github.com/michaldror26/Final-Project-2019-Server
    # Conflicts:
    #	FinalProject2018/API/API.csproj
    #	FinalProject2018/API/Controllers/userController.cs
    #	FinalProject2018/API/bin/API.dll
    #	FinalProject2018/API/bin/API.pdb
    #	FinalProject2018/API/bin/BLL.dll
    #	FinalProject2018/API/bin/BLL.pdb
    #	FinalProject2018/API/bin/DAL.dll
    #	FinalProject2018/API/bin/DAL.pdb
    #	FinalProject2018/API/bin/Entities.dll
    #	FinalProject2018/API/bin/Entities.pdb
    #	FinalProject2018/API/obj/Debug/API.dll
    #	FinalProject2018/API/obj/Debug/API.pdb
    #	FinalProject2018/API/obj/Debug/DesignTimeResolveAssemblyReferencesInput.cache
    #	FinalProject2018/BLL/BLL.csproj
    #	FinalProject2018/BLL/UserSiteService.cs
    #	FinalProject2018/BLL/bin/Debug/BLL.dll
    #	FinalProject2018/BLL/bin/Debug/BLL.pdb
    #	FinalProject2018/BLL/bin/Debug/DAL.dll
    #	FinalProject2018/BLL/bin/Debug/DAL.pdb
    #	FinalProject2018/BLL/bin/Debug/Entities.dll
    #	FinalProject2018/BLL/bin/Debug/Entities.pdb
    #	FinalProject2018/BLL/obj/Debug/BLL.dll
    #	FinalProject2018/BLL/obj/Debug/BLL.pdb
    #	FinalProject2018/BLL/obj/Debug/DesignTimeResolveAssemblyReferencesInput.cache
    #	FinalProject2018/DAL/bin/Debug/DAL.dll
    #	FinalProject2018/DAL/bin/Debug/DAL.pdb
    #	FinalProject2018/DAL/bin/Debug/Entities.dll
    #	FinalProject2018/DAL/bin/Debug/Entities.pdb
    #	FinalProject2018/DAL/obj/Debug/DAL.csproj.FileListAbsolute.txt
    #	FinalProject2018/DAL/obj/Debug/DAL.csproj.GenerateResource.Cache
    #	FinalProject2018/DAL/obj/Debug/DAL.dll
    #	FinalProject2018/DAL/obj/Debug/DAL.pdb
    #	FinalProject2018/Entities/Employee.cs
    #	FinalProject2018/Entities/Product.cs
    #	FinalProject2018/Entities/User.cs
    #	FinalProject2018/Entities/bin/Debug/Entities.dll
    #	FinalProject2018/Entities/bin/Debug/Entities.pdb
    #	FinalProject2018/Entities/obj/Debug/Entities.dll
    #	FinalProject2018/Entities/obj/Debug/Entities.pdb

---
+++ FinalProject2018/Entities/Admin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    [Table("admin")]
+    public class Admin:User
+    {
+        [Key]
+        public int AdminId { get; set; }
+        //[ForeignKey("SiteUser")]
+        //public Nullable<int> SiteUserId { get; set; }
+        //public virtual SiteUser SiteUser { get; set; }
+
+    }
+}

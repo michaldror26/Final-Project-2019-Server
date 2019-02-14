ece5e64f3bc87f59ce4a38e87dfba74be76ae108
michaldror26 <michaldror26@gmail.com>
2019-02-15 01:54:25 +0200

    contact us

---
+++ FinalProject2018/API/Controllers/ContactUs.cs
@@ -0,0 +1,35 @@
+﻿using BLL;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace API.Controllers
+{
+    [RoutePrefix("api/contactUs")]
+    [EnableCors("*", "*", "*")]
+    public class ContactUsController : ApiController
+    {
+        MailViaGmailService sendMailService;
+        MailboxMessageService mailboxMessageService;
+
+        public ContactUsController()
+        {
+            sendMailService = new MailViaGmailService();
+            mailboxMessageService = new MailboxMessageService();
+        }
+
+        [HttpPost]
+        [Route("sendMailToAdmin")]
+        public MailboxMessage sendMailToAdmin([FromBody()]MailboxMessage message)
+        {
+            mailboxMessageService.AddMailboxMessage(message);
+            return sendMailService.sendContactUs(message);
+        }
+        
+    }
+}

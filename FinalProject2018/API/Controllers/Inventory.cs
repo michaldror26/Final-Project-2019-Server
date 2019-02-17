b63728b5808e5b1b183188d0c538736e8f7ea49e
michaldror26 <michaldror26@gmail.com>
2019-02-17 23:40:27 +0200

    inventory

---
+++ FinalProject2018/API/Controllers/Inventory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using BLL;
+using Entities;
+
+namespace API.Controllers
+{
+    [RoutePrefix("api/inventory")]
+    [EnableCors("*", "*", "*")]
+    public class InventoryController : ApiController
+    {
+        InventoryService service;
+        public InventoryController()
+        {
+            service = new InventoryService();
+        }
+
+        [HttpGet]
+        [Route("getInventory")]
+        public List<Inventory> get()
+        {
+            return service.getInventory();
+        }
+        
+    }
+}
